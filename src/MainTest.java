eb0832b871d9b64e7d1a89dffc0ba91a2ffdd582
norog6 <92385720+norog6@users.noreply.github.com>
2023-07-18 16:27:31 +0300

    tests

--- src/MainTest.java
+++ src/MainTest.java
@@ -5,12 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
-    private static final String attempt="asdsa";
-    private static final String word="asdsa";
-    private static final Color[] colors=new Color[]{Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN};
+    private static final String attempt = "asdsa";
+    private static final String word = "asdsa";
+    private static final Color[] colors = new Color[]{Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN};
+
     @Test
     @Order(1)
     void main() {
-        assertArrayEquals( Main.wordle(attempt,word),colors);
+        assertArrayEquals(Main.wordle(attempt, word), colors);
     }
 }
